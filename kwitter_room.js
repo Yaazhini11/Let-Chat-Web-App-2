72e039d89482e4256dca4aef346ea340c9c72309
Yaazhini11 <85733759+Yaazhini11@users.noreply.github.com>
2021-08-17 14:55:23 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAR07UXpueLgTQTd_QADOhjAdufhJljmXE",
+    authDomain: "emeralds-and-rubies-4bcef.firebaseapp.com",
+    databaseURL: "https://emeralds-and-rubies-4bcef-default-rtdb.firebaseio.com",
+    projectId: "emeralds-and-rubies-4bcef",
+    storageBucket: "emeralds-and-rubies-4bcef.appspot.com",
+    messagingSenderId: "824193603940",
+    appId: "1:824193603940:web:a3c8c835f39c79be190271"
+}
